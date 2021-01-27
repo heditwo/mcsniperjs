16344c8c751b86e46ce4141fefd23a9246f18f8c
Nicholas <nicholasbarcelo@icloud.com>
2021-01-27 08:46:32 -0500

    log to file

--- utils/logger.js
+++ utils/logger.js
@@ -1,24 +1,31 @@
 const chalk = require('chalk')
+const fs = require('fs')
+const log = fs.createWriteStream('log.txt', { flags: 'a' })
 
 const info = (msg) => {
     console.log(chalk.keyword('orange')(`[${new Date().toISOString()}]`) + chalk.blue(' [INFO]: ') + msg)
+    log.write(`[${new Date().toISOString()}] [INFO]: ${msg}\n`)
 }
 
 const warn = (msg) => {
     console.log(chalk.keyword('orange')(`[${new Date().toISOString()}]`) + chalk.yellow(' [WARN]: ') + msg)
+    log.write(`[${new Date().toISOString()}] [WARN]: ${msg}\n`)
 }
 
 const error = (msg) => {
     console.log(chalk.keyword('orange')(`[${new Date().toISOString()}]`) + chalk.red(' [ERROR]: ') + msg)
+    log.write(`[${new Date().toISOString()}] [ERROR]: ${msg}\n`)
     process.exit()
 }
 
 const fail = (msg) => {
     console.log(chalk.keyword('orange')(`[${new Date().toISOString()}]`) + chalk.red(' [FAIL]: ') + msg)
+    log.write(`[${new Date().toISOString()}] [FAIL]: ${msg}\n`)
 }
 
 const success = (msg) => {
     console.log(chalk.keyword('orange')(`[${new Date().toISOString()}]`) + chalk.green(' [SUCCESS]: ') + msg)
+    log.write(`[${new Date().toISOString()}] [SUCCESS]: ${msg}\n`)
     process.exit()
 }
 
